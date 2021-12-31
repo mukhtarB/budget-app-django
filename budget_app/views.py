dd7fb3025cba7f3f82ecc57951a4482aacc6a9fe
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-12-31 10:27:52 +0100

    set up views & templates

--- budget_app/views.py
+++ budget_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def project_list(request):
-    return render(request, 'budget/project_list.html')
+    return render(request, 'budget_app/project_list.html')
 
 
 def project_detail(request, project_slug):
-    return render(request, 'budget/project_detail.html')
+    # fetching the correct project
+    return render(request, 'budget_app/project_detail.html')
