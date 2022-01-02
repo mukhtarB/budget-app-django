2cb6ea9bf809d7ddfeaefa361fd4a3913664d9a9
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-01-02 08:16:39 +0100

    fetching project for details page

--- budget_app/views.py
+++ budget_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+# models
+from .models import Project
 
 # Create your views here.
 
@@ -9,4 +12,11 @@ def project_list(request):
 
 def project_detail(request, project_slug):
     # fetching the correct project
-    return render(request, 'budget_app/project_detail.html')
+
+    # project = Project.objects.get(slug = project_slug)
+    project = get_object_or_404(Project, slug=project_slug)
+
+    context = {
+        'project': project
+    }
+    return render(request, 'budget_app/project_detail.html', context)
