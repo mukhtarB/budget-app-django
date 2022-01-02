6ac53acfd0832bdda39256409095ba3b23ee2cc9
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-01-02 08:43:44 +0100

    + cbv: Project-create-view

--- budget_app/urls.py
+++ budget_app/urls.py
@@ -20,4 +20,5 @@ from . import views
 urlpatterns = [
     path('', views.project_list, name='list'),
     path('<slug:project_slug>', views.project_detail, name='detail'),
+    path('add', views.ProjectCreateView.as_view(), name='add'),
 ]
--- budget_app/views.py
+++ budget_app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView
 
 # models
 from .models import Project
-
 # Create your views here.
-
 
 def project_list(request):
     return render(request, 'budget_app/project_list.html')
@@ -14,10 +13,16 @@ def project_detail(request, project_slug):
     # fetching the correct project
 
     # project = Project.objects.get(slug = project_slug)
-    project = get_object_or_404(Project, slug=project_slug)
+    project =  get_object_or_404(Project, slug=project_slug)
 
     context = {
         'project': project,
         'expense_list': project.expenses.all()
     }
     return render(request, 'budget_app/project_detail.html', context)
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    template_name = ' budget_app/add-project.html'
+    fields = ('name', 'budget')
