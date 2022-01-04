865c663e6b2f10b9eec5142a8fbe0314ccb73a0e
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-01-04 12:40:02 +0100

    url test resolved

--- budget_app/tests/tests_urls.py
+++ budget_app/tests/tests_urls.py
@@ -1,6 +1,10 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from ..views import project_list
+from ..views import (
+    ProjectCreateView,
+    project_detail,
+    project_list
+)
 
 
 class TestUrls(SimpleTestCase):
@@ -8,3 +12,11 @@ class TestUrls(SimpleTestCase):
     def test_list_url_resolves(self):
         url = reverse('list')
         self.assertEqual(resolve(url).func, project_list)
+
+    def test_detail_url_resolves(self):
+        url = reverse('detail', args=['random-slug'])
+        self.assertEqual(resolve(url).func, project_detail)
+
+    def test_add_project_url_resolves(self):
+        url = reverse('add')
+        self.assertEqual(resolve(url).func.view_class, ProjectCreateView)
