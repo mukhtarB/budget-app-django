71f2a30d9b339c1b8e3ad43d61aa991170b3f28f
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-01-04 08:07:24 +0100

    set expense ordering && fixed styling

--- budget_app/models.py
+++ budget_app/models.py
@@ -50,5 +50,8 @@ class Expense(models.Model):
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ('-amount',)
+
     def __str__(self):
         return self.title
