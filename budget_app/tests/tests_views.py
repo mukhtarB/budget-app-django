57e10c2fbc10345e69f9853ce8bdb80c3a225923
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-01-05 09:48:05 +0100

    get request - porject_detail view

--- budget_app/tests/tests_views.py
+++ budget_app/tests/tests_views.py
@@ -1,7 +1,9 @@
 from django.http import response
 from django.test import TestCase, Client, client
 from django.urls import reverse
-# from budget_app.models import ()
+from budget_app.models import (
+    Project,
+)
 
 # setup codes
 # test codes
@@ -13,6 +15,13 @@ class TestViews(TestCase):
     def setUp(self) -> None:
         self.client = Client()
         self.list_url = reverse('list')
+        self.detail_url = reverse('detail', args=['project1'])
+
+        self.project = Project.objects.create(
+            name='project1',
+            budget=7000
+        )
+
         return super().setUp()
 
     def test_project_list_GET(self):
@@ -20,3 +29,9 @@ class TestViews(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'budget_app/project_list.html')
+
+    def test_project_detail_GET(self):
+        response = self.client.get(self.detail_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'budget_app/project_detail.html')
