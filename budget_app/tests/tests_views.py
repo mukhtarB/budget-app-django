3cfb38739c87ed5352e8487b37a5579fc941d388
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-01-05 10:02:40 +0100

    + project_detail test add expense

--- budget_app/tests/tests_views.py
+++ budget_app/tests/tests_views.py
@@ -2,8 +2,10 @@ from django.http import response
 from django.test import TestCase, Client, client
 from django.urls import reverse
 from budget_app.models import (
+    Category,
     Project,
 )
+from budget_app.views import project_list
 
 # setup codes
 # test codes
@@ -22,6 +24,11 @@ class TestViews(TestCase):
             budget=7000
         )
 
+        self.category = Category.objects.create(
+            project=self.project,
+            name='development_testing'
+        )
+
         return super().setUp()
 
     def test_project_list_GET(self):
@@ -35,3 +42,15 @@ class TestViews(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'budget_app/project_detail.html')
+
+    def test_project_detail_POST_add_expense(self):
+        response = self.client.post(self.detail_url, {
+            'title': 'expense1',
+            'amount': 1000,
+            'category': 'development_testing'
+        })
+
+        print(self.project.expenses)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.project.expenses.first().title, 'expense1')
